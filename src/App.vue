c1790f59e336583e340bfafee72043c37bc84108
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-01-18 17:48:12 +0700

    add Readme File and simple proejct

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-neutral">Neutral</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-ghost">Ghost</button>
+<button className="btn btn-active btn-link">Link</button>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
