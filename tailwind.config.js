611b4dab75b902f5eba9a02da1b97f31b5edce0d
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-01-28 17:48:35 +0700

    add timer and slider how to play

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        itim: "'Itim', cursive",
+        istok: "'Istok Web', sans-serif"
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
