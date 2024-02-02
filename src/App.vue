de3d5c4d1ad20a9614fac2071dc0a553492ecf91
carrynong2 <carrynong2@gmail.com>
2024-02-02 21:10:05 +0700

    Revert "add state of app"
    This reverts commit 621597f282b970e540b08e1108a8e819a4072c64.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-  <script setup>
-import { computed, ref } from "vue";
+<script setup>
+import { ref } from "vue";
 
 // data mockup สำหรับการ test rule componant
 
 let passwordRules = [
+
+let rules = [
   {
     level: "hard",
     rules: [
@@ -26,16 +28,9 @@ let passwordRules = [
     ],
   },
   {
-    level: "hardest",
-    rules: [
-      { id: 3, message: "Your neet to love your cat", correct: true },
-      { id: 2, message: "Your don't loved your catr", correct: false },
-      {
-        id: 1,
-        message: "Your loved your cat",
-        correct: true,
-      },
-    ],
+    ruleNumber: 1,
+    message: "Your password must be at least 5 characters",
+    correct: true,
   },
 ];
 
@@ -97,37 +92,31 @@ function Displaytimeformat() {
 
   return `${hours}:${minutes}:${seconds}`;
 }
+function levelSelector(level) {
+  if (level === "hard") {
+    console.warn("HARD MODE");
+  } else if (level === "veryhard") {
+    console.warn("VERY HARD MODE");
+  } else console.warn("HARDEST MODE!");
+  if (level === "hard") {
+    console.warn("HARD MODE");
+  } else if (level === "veryhard") {
+    console.warn("VERY HARD MODE");
+  } else console.warn("HARDEST MODE!");
+}
 </script>
 
-  <template>
+<template>
   <!-- rulebox componant -->
   <div
-    :class="
-      selectedLevel === 'hard'
-        ? 'background-color-hard'
-        : selectedLevel === 'veryhard'
-        ? 'background-color-veryhard'
-        : selectedLevel === 'hardest'
-        ? 'btn-bg-hardest'
-        : 'background-color-hard'
-    "
-    class="flex flex-col w-full min-h-screen items-center"
+    class="background-color-hard flex flex-col w-full min-h-screen items-center"
   >
     <img
       src="./assets/logo/IMG_5174-removebg-preview.png"
       class="mobile:flex w-3/5 h-3/5 my-4 logo"
     />
     <div
-      :class="
-        selectedLevel === 'hard'
-          ? 'bg-color-hard-box'
-          : selectedLevel === 'veryhard'
-          ? 'bg-color-veryhard-box'
-          : selectedLevel === 'hardest'
-          ? 'bg-color-hardest-box'
-          : 'bg-color-hard-box'
-      "
-      class="flex flex-col items-center w-11/12 h-full rounded-box p-3 hardBox"
+      class="flex flex-col items-center w-11/12 h-full bg-color-hard-box rounded-box p-3 hardBox"
     >
       <section
         id="select_level"
@@ -144,6 +133,16 @@ function Displaytimeformat() {
             >
               HARD
             </button>
+            <p class="font-Saira text-white text-center font-medium">
+              SELECT LEVEL
+            </p>
+            <button
+              @click="levelSelector('hard')"
+              class="font-Saira text-md text-center font-medium motion-safe:hover:scale-110 text-white h-20 w-20 focus:scale-110 rounded-full btn-bg-hard min-w-max transition-all ease-linear cursor-pointer border-2 border-black hover:border-white"
+            >
+              HARD
+            </button>
+
             <button
               @click="levelSelector('veryhard')"
               class="font-Saira text-md text-center font-medium text-white h-20 w-20 rounded-full btn-bg-veryHard shadow-lg transition-all hover:shadow-red-500/50 motion-safe:hover:scale-110 focus:scale-110 my-3 mx-2"
@@ -157,6 +156,18 @@ function Displaytimeformat() {
               HARDEST
             </button>
           </div>
+          <button
+            @click="levelSelector('veryhard')"
+            class="font-Saira text-md text-center font-medium motion-safe:hover:scale-110 text-white h-20 w-20 focus:scale-110 rounded-full bg-blue-300 min-w-max transition-all border-2 border-black hover:border-white"
+          >
+            VERY<br />HARD
+          </button>
+          <button
+            @click="levelSelector('hardest')"
+            class="font-Saira text-md text-center font-medium motion-safe:hover:scale-110 text-white h-20 w-20 focus:scale-110 rounded-full bg-emerald-400 min-w-max transition-all border-2 border-black hover:border-white"
+          >
+            HARDEST
+          </button>
         </div>
       </section>
       <div id="input-password" class="items-center w-[300px]">
@@ -172,14 +183,13 @@ function Displaytimeformat() {
             class="font-itim text-[14px] input input-bordered w-full max-w-xs bg-[#FAFAFA] shadow-inner-lx"
             @input="startGame"
             v-model="userInput"
+            @click="startTimer"
           />
         </label>
       </div>
       <p class="font-Saira text-[14px] text-white mt-[10px]">
         Time:
-        <span class="text-[14px]">
-          {{ Displaytimeformat() }}
-        </span>
+        <span class="text-[14px] text-red">{{ Displaytimeformat() }}</span>
       </p>
       <div class="mobile:flex w-[300px] flex-col my-7 items-center spy">
         <img
@@ -236,6 +246,10 @@ function Displaytimeformat() {
               ? "HACKER"
               : "SPY"
           }}
+      <div class="mobile:flex flex-col items-center spy">
+        <img class="" src="./assets/picture/hard-pic.png" />
+        <p class="font-Saira text-[13px] text-white mt-[5px]">
+          Your Character : SPY
         </p>
       </div>
       <div
@@ -284,76 +298,101 @@ function Displaytimeformat() {
         </div>
       </div>
 
+      <!-- <div class="grid md:w-4/12 grid-cols-1 m-2 gap-5">
+        <div
+          v-for="rule in rules"
+          class="sm:w-full rounded-md shadow-md bg-red-300 p-0.5"
+          :key="rule.ruleNumber"
+        >
+          <div
+            :class="rule.correct ? 'bg-green-300' : 'bg-red-300'"
+            class="py-1 px-4 flex flex-row items-center gap-2"
+          >
+            <Correct
+              v-if="rule.correct"
+              class="fa-solid fa-check text-red-500 pt-1 text-2xl"
+            />
+            <i v-else class="fa-solid fa-xmark text-red-500 pt-1 text-2xl"></i>
+            <p class="font-itim text-xl">Rule {{ rule.ruleNumber }}</p>
+          </div>
+          <div
+            :class="rule.correct ? 'bg-green-100' : 'bg-red-100'"
+            class="p-4"
+          >
+            <p class="font-itim text-xl">{{ rule.message }}.</p>
+          </div>
+        </div> -->
+      <!-- </div> -->
       <!-- Timer display -->
       <!-- <div class="mt-5">
-          <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
-          button for test timer-->
+        <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
+        button for test timer-->
       <!-- <button
-            @click="startTimer"
-            class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
-          >
-            Start Timer
-          </button>
-          <button
-            @click="stopTimer"
-            class="font-itim border border-black p-1 rounded-[5px]"
-          >
-            Stop Timer
-          </button>
-        </div> -->
-      <!-- <div
-          :class="
-            isOpen
-              ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
-              : 'h-[43px] transition-height duration-300 ease-in-out'
-          "
-          class="fixed bottom-0 overflow-hidden"
+          @click="startTimer"
+          class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
         >
-          <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
-            <button class="flex flex-col items-center font-itim">
-              HOW TO PLAY GAME 🎮
-              <svg
-                width="24"
-                height="10"
-                viewBox="0 0 27 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <line
-                  x1="13.8826"
-                  y1="15.2968"
-                  x2="1.29294"
-                  y2="2.70715"
-                  stroke="black"
-                  stroke-width="2"
-                />
-                <line
-                  x1="13.0877"
-                  y1="14.5877"
-                  x2="26.0877"
-                  y2="1.58769"
-                  stroke="black"
-                  stroke-width="2"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="w-[300px] p-[10px]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
-              magni repudiandae tempora eos nemo maiores, doloremque quis
-              obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
-              Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
-              incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
-              Maxime aliquam eos excepturi vel.
-            </p>
-          </div> -->
+          Start Timer
+        </button>
+        <button
+          @click="stopTimer"
+          class="font-itim border border-black p-1 rounded-[5px]"
+        >
+          Stop Timer
+        </button>
+      </div> -->
+      <!-- <div
+        :class="
+          isOpen
+            ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
+            : 'h-[43px] transition-height duration-300 ease-in-out'
+        "
+        class="fixed bottom-0 overflow-hidden"
+      >
+        <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
+          <button class="flex flex-col items-center font-itim">
+            HOW TO PLAY GAME 🎮
+            <svg
+              width="24"
+              height="10"
+              viewBox="0 0 27 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="13.8826"
+                y1="15.2968"
+                x2="1.29294"
+                y2="2.70715"
+                stroke="black"
+                stroke-width="2"
+              />
+              <line
+                x1="13.0877"
+                y1="14.5877"
+                x2="26.0877"
+                y2="1.58769"
+                stroke="black"
+                stroke-width="2"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="w-[300px] p-[10px]">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
+            magni repudiandae tempora eos nemo maiores, doloremque quis
+            obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
+            Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
+            incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
+            Maxime aliquam eos excepturi vel.
+          </p>
+        </div> -->
     </div>
   </div>
 </template>
 
-  <style scoped>
+<style scoped>
 .background-color-hard {
   background: linear-gradient(
     104deg,
@@ -364,31 +403,19 @@ function Displaytimeformat() {
 }
 
 .bg-color-hard-box {
+.bg-color-hard-box {
   border-radius: 31px;
   border: 1px solid #fff;
   background: #590ebb;
 }
 
 .btn-bg-hard {
+.btn-bg-hard {
   background: linear-gradient(
     104deg,
     #590ebb 6.68%,
     rgba(0, 0, 0, 0.74) 92.15%
   );
-}
-
-.background-color-veryhard {
-  background: linear-gradient(
-    104deg,
-    rgba(209, 164, 15, 1) 0%,
-    rgba(133, 39, 18, 1) 100%
-  );
-}
-
-.bg-color-veryhard-box {
-  border-radius: 31px;
-  border: 1px solid #fff;
-  background: #e36409;
 }
 
 .btn-bg-veryHard {
@@ -399,23 +426,6 @@ function Displaytimeformat() {
     rgba(60, 23, 8, 0.68) 89.63%
   );
 }
-
-.background-color-hardest {
-  background: linear-gradient(
-    104deg,
-    rgba(209, 15, 15, 1) 14%,
-    rgba(133, 18, 18, 1) 87%,
-    rgba(209, 164, 15, 1) 100%,
-    rgba(133, 18, 18, 1) 100%
-  );
-}
-
-.bg-color-hardest-box {
-  border-radius: 31px;
-  border: 1px solid #fff;
-  background: #cb0809;
-}
-
 .btn-bg-hardest {
   background: linear-gradient(
     104deg,
@@ -424,7 +434,6 @@ function Displaytimeformat() {
     rgba(29, 34, 45, 0.94) 89.63%
   );
 }
-
 @media (max-width: 600px) {
   .HowToPlay {
     height: 100vh;
