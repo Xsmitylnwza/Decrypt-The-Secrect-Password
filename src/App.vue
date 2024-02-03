6f998d5f24c58235afb649c68285b8555557f8d9
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-02-03 20:34:32 +0700

    Add clean code, dynamic question, dynamic checkAnswer

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,118 @@
-<script setup>
-import { ref } from "vue"
+  <script setup>
+import { computed, ref } from "vue";
 
 // data mockup สำหรับการ test rule componant
 
-let rules = [
+let selectedLevel = ref("");
+let userInput = ref("");
+
+let passwordRules = [
   {
-    ruleNumber: 2,
-    message: "Your password must include a number",
-    correct: false
+    level: "hard",
+    rules: [
+      {
+        id: 1,
+        message: "Your loved your cat",
+        correct: true,
+      },
+    ],
   },
   {
-    ruleNumber: 1,
-    message: "Your password must be at least 5 characters",
-    correct: true
-  }
-]
+    level: "veryhard",
+    rules: [
+      { id: 2, message: "Your don't loved your cat", correct: false },
+      {
+        id: 1,
+        message: "Your loved your cat",
+        correct: true,
+      },
+    ],
+  },
+  {
+    level: "hardest",
+    rules: [
+      { id: 3, message: "Your neet to love your cat", correct: true },
+      { id: 2, message: "Your don't loved your catr", correct: false },
+      {
+        id: 1,
+        message: "Your loved your cat",
+        correct: true,
+      },
+    ],
+  },
+];
 
-let isOpen = ref(false)
+function levelSelector(level) {
+  selectedLevel.value = level;
+}
+
+const selectedRules = computed(() => {
+  const level = selectedLevel.value;
+  const selectedRules = passwordRules.find((rule) => rule.level === level);
+  return selectedRules ? selectedRules.rules : [];
+});
+
+let isOpen = ref(false);
 
 // Timer function handle
-let timer = ref(0)
-let timerInterval
+let timer = ref(0);
+let timerInterval;
 
 // function สำหรับจับเวลาจะอัพเดทค่าทุกๆ 1 วินาที
 function startTimer() {
   timerInterval = setInterval(() => {
-    timer.value++
-  }, 1000)
+    timer.value++;
+  }, 1000);
 }
 
 // function สำหรับหยุดจับเวลา
 function stopTimer() {
-  clearInterval(timerInterval)
+  clearInterval(timerInterval);
 }
 
 // function สำหรับแสดงผลลัพธ์ของเวลา
 function Displaytimeformat() {
   const hours = Math.floor(timer.value / 3600)
     .toString()
-    .padStart(2, "0")
+    .padStart(2, "0");
   const minutes = Math.floor((timer.value % 3600) / 60)
     .toString()
-    .padStart(2, "0")
-  const seconds = (timer.value % 60).toString().padStart(2, "0")
+    .padStart(2, "0");
+  const seconds = (timer.value % 60).toString().padStart(2, "0");
 
-  return `${hours}:${minutes}:${seconds}`
-}
-function levelSelector(level) {
-  if (level === "hard") {
-    console.warn("HARD MODE")
-  } else if (level === "veryhard") {
-    console.warn("VERY HARD MODE")
-  } else console.warn("HARDEST MODE!")
+  return `${hours}:${minutes}:${seconds}`;
 }
 </script>
 
-<template>
+  <template>
   <!-- rulebox componant -->
   <div
-    class="background-color-hard flex flex-col w-full min-h-screen items-center"
+    :class="
+      selectedLevel === 'hard'
+        ? 'background-color-hard'
+        : selectedLevel === 'veryhard'
+        ? 'background-color-veryhard'
+        : selectedLevel === 'hardest'
+        ? 'btn-bg-hardest'
+        : 'background-color-hard'
+    "
+    class="flex flex-col w-full min-h-screen items-center"
   >
     <img
       src="./assets/logo/IMG_5174-removebg-preview.png"
       class="mobile:flex w-3/5 h-3/5 my-4 logo"
     />
     <div
-      class="flex flex-col items-center w-11/12 h-full bg-color-hard-box rounded-box p-3 hardBox"
+      :class="
+        selectedLevel === 'hard'
+          ? 'bg-color-hard-box'
+          : selectedLevel === 'veryhard'
+          ? 'bg-color-veryhard-box'
+          : selectedLevel === 'hardest'
+          ? 'bg-color-hardest-box'
+          : 'bg-color-hard-box'
+      "
+      class="flex flex-col items-center w-11/12 h-full rounded-box p-3 hardBox"
     >
       <section
         id="select_level"
@@ -82,7 +129,6 @@ function levelSelector(level) {
             >
               HARD
             </button>
-
             <button
               @click="levelSelector('veryhard')"
               class="font-Saira text-md text-center font-medium text-white h-20 w-20 rounded-full btn-bg-veryHard shadow-lg transition-all hover:shadow-red-500/50 motion-safe:hover:scale-110 focus:scale-110 my-3 mx-2"
@@ -109,18 +155,68 @@ function levelSelector(level) {
             type="text"
             placeholder="Type here"
             class="font-itim text-[14px] input input-bordered w-full max-w-xs bg-[#FAFAFA] shadow-inner-lx"
-            @click="startTimer"
+            v-model="userInput"
           />
         </label>
       </div>
       <p class="font-Saira text-[14px] text-white mt-[10px]">
         Time:
-        <span class="text-[14px] text-red">{{ Displaytimeformat() }}</span>
+        <span class="text-[14px]">
+          {{ Displaytimeformat() }}
+        </span>
       </p>
-      <div class="mobile:flex flex-col items-center spy">
-        <img class="" src="./assets/picture/hard-pic.png" />
-        <p class="font-Saira text-[13px] text-white mt-[5px]">
-          Your Character : SPY
+      <div class="mobile:flex w-[300px] flex-col my-7 items-center spy">
+        <img
+          v-if="selectedLevel === 'hard' && !userInput"
+          class=""
+          src="./assets/picture/hard-pic.png"
+        />
+        <img
+          v-if="selectedLevel === 'veryhard' && !userInput"
+          class=""
+          src="./assets/picture/very-hard-pic.png"
+        />
+        <img
+          v-if="selectedLevel === 'hardest' && !userInput"
+          class="w-80 h-52"
+          src="./assets/picture/hardest-pic.png"
+        />
+        <img
+          v-if="selectedLevel === '' && !userInput"
+          class=""
+          src="./assets/picture/hard-pic.png"
+        />
+        <div v-if="userInput" class="flex flex-col">
+          <div
+            v-for="rule in selectedRules"
+            class="sm:w-full rounded-md py-4"
+            :key="rule.id"
+          >
+            <div
+              :class="rule.correct ? 'bg-green-300' : 'bg-red-300'"
+              class="py-1 px-4 flex flex-row items-center gap-2"
+            >
+              <i
+                v-if="rule.correct"
+                class="fa-solid fa-check text-red-500 pt-1 text-xl"
+              />
+              <i v-else class="fa-solid fa-xmark text-red-500 pt-1 text-xl"></i>
+              <p class="font-itim text-sm">Rule {{ rule.id }}</p>
+              <p class="font-itim text-sm">{{ rule.message }}.</p>
+            </div>
+          </div>
+        </div>
+        <p v-if="!userInput" class="font-Saira text-[13px] text-white mt-[5px]">
+          Your Character :
+          {{
+            selectedLevel === "hard"
+              ? "SPY"
+              : selectedLevel === "veryhard"
+              ? "FBI"
+              : selectedLevel === "hardest"
+              ? "HACKER"
+              : "SPY"
+          }}
         </p>
       </div>
       <div
@@ -169,102 +265,76 @@ function levelSelector(level) {
         </div>
       </div>
 
-      =
-      <!-- <div class="grid md:w-4/12 grid-cols-1 m-2 gap-5">
-        <div
-          v-for="rule in rules"
-          class="sm:w-full rounded-md shadow-md bg-red-300 p-0.5"
-          :key="rule.ruleNumber"
-        >
-          <div
-            :class="rule.correct ? 'bg-green-300' : 'bg-red-300'"
-            class="py-1 px-4 flex flex-row items-center gap-2"
-          >
-            <Correct
-              v-if="rule.correct"
-              class="fa-solid fa-check text-red-500 pt-1 text-2xl"
-            />
-            <i v-else class="fa-solid fa-xmark text-red-500 pt-1 text-2xl"></i>
-            <p class="font-itim text-xl">Rule {{ rule.ruleNumber }}</p>
-          </div>
-          <div
-            :class="rule.correct ? 'bg-green-100' : 'bg-red-100'"
-            class="p-4"
-          >
-            <p class="font-itim text-xl">{{ rule.message }}.</p>
-          </div>
-        </div> -->
-      <!-- </div> -->
       <!-- Timer display -->
       <!-- <div class="mt-5">
-        <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
-        button for test timer-->
+          <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
+          button for test timer-->
       <!-- <button
-          @click="startTimer"
-          class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
-        >
-          Start Timer
-        </button>
-        <button
-          @click="stopTimer"
-          class="font-itim border border-black p-1 rounded-[5px]"
-        >
-          Stop Timer
-        </button>
-      </div> -->
-      <!-- <div
-        :class="
-          isOpen
-            ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
-            : 'h-[43px] transition-height duration-300 ease-in-out'
-        "
-        class="fixed bottom-0 overflow-hidden"
-      >
-        <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
-          <button class="flex flex-col items-center font-itim">
-            HOW TO PLAY GAME 🎮
-            <svg
-              width="24"
-              height="10"
-              viewBox="0 0 27 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line
-                x1="13.8826"
-                y1="15.2968"
-                x2="1.29294"
-                y2="2.70715"
-                stroke="black"
-                stroke-width="2"
-              />
-              <line
-                x1="13.0877"
-                y1="14.5877"
-                x2="26.0877"
-                y2="1.58769"
-                stroke="black"
-                stroke-width="2"
-              />
-            </svg>
+            @click="startTimer"
+            class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
+          >
+            Start Timer
           </button>
-        </div>
-        <div class="w-[300px] p-[10px]">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
-            magni repudiandae tempora eos nemo maiores, doloremque quis
-            obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
-            Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
-            incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
-            Maxime aliquam eos excepturi vel.
-          </p>
+          <button
+            @click="stopTimer"
+            class="font-itim border border-black p-1 rounded-[5px]"
+          >
+            Stop Timer
+          </button>
         </div> -->
+      <!-- <div
+          :class="
+            isOpen
+              ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
+              : 'h-[43px] transition-height duration-300 ease-in-out'
+          "
+          class="fixed bottom-0 overflow-hidden"
+        >
+          <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
+            <button class="flex flex-col items-center font-itim">
+              HOW TO PLAY GAME 🎮
+              <svg
+                width="24"
+                height="10"
+                viewBox="0 0 27 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="13.8826"
+                  y1="15.2968"
+                  x2="1.29294"
+                  y2="2.70715"
+                  stroke="black"
+                  stroke-width="2"
+                />
+                <line
+                  x1="13.0877"
+                  y1="14.5877"
+                  x2="26.0877"
+                  y2="1.58769"
+                  stroke="black"
+                  stroke-width="2"
+                />
+              </svg>
+            </button>
+          </div>
+          <div class="w-[300px] p-[10px]">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
+              magni repudiandae tempora eos nemo maiores, doloremque quis
+              obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
+              Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
+              incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
+              Maxime aliquam eos excepturi vel.
+            </p>
+          </div> -->
     </div>
   </div>
 </template>
 
-<style scoped>
+  <style scoped>
 .background-color-hard {
   background: linear-gradient(
     104deg,
@@ -288,6 +358,20 @@ function levelSelector(level) {
   );
 }
 
+.background-color-veryhard {
+  background: linear-gradient(
+    104deg,
+    rgba(209, 164, 15, 1) 0%,
+    rgba(133, 39, 18, 1) 100%
+  );
+}
+
+.bg-color-veryhard-box {
+  border-radius: 31px;
+  border: 1px solid #fff;
+  background: #e36409;
+}
+
 .btn-bg-veryHard {
   background: linear-gradient(
     104deg,
@@ -296,6 +380,23 @@ function levelSelector(level) {
     rgba(60, 23, 8, 0.68) 89.63%
   );
 }
+
+.background-color-hardest {
+  background: linear-gradient(
+    104deg,
+    rgba(209, 15, 15, 1) 14%,
+    rgba(133, 18, 18, 1) 87%,
+    rgba(209, 164, 15, 1) 100%,
+    rgba(133, 18, 18, 1) 100%
+  );
+}
+
+.bg-color-hardest-box {
+  border-radius: 31px;
+  border: 1px solid #fff;
+  background: #cb0809;
+}
+
 .btn-bg-hardest {
   background: linear-gradient(
     104deg,
@@ -304,6 +405,7 @@ function levelSelector(level) {
     rgba(29, 34, 45, 0.94) 89.63%
   );
 }
+
 @media (max-width: 600px) {
   .HowToPlay {
     height: 100vh;
