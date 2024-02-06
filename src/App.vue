e0c9fd0f26710fd8a5e947b257daa5f81cace34a
Tiskas <saksit.tatitrisakul@gmail.com>
2024-02-06 14:36:05 +0700

    editer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { ref } from 'vue'
-
+import { computed, ref } from "vue"
 
 // data mockup สำหรับการ test rule componant
 
@@ -73,7 +72,12 @@ const checkAnswer = {
 }
 
 
-
+function levelSelector(level) {
+  selectedLevel.value = level;
+  passedRule.value = 1;
+  stopTimer();
+  resetGame();
+}
 
 function checkAnswerHard() {
   let question = passwordRules[0]
@@ -163,13 +167,6 @@ function Displaytimeformat() {
 
   return `${hours}:${minutes}:${seconds}`;
 }
-function levelSelector(level) {
-  if (level === "hard") {
-    console.warn("HARD MODE")
-  } else if (level === "veryhard") {
-    console.warn("VERY HARD MODE")
-  } else console.warn("HARDEST MODE!")
-}
 </script>
 
 <template>
@@ -241,7 +238,7 @@ function levelSelector(level) {
       <div class="font-Saira text-base text-white pt-6 animate-bounce ml-[5px] HowToPlayFont">
         HOW TO PLAY GAME
       </div>
-      <div class="h-screen">
+      <div class="HowToPlay">
         <a class="flex justify-center" href="#HowtoPlay">
           <svg class="animate-bounce w-8 h-8" width="27" height="16" viewBox="0 0 27 16" fill="none"
             xmlns="http://www.w3.org/2000/svg">
@@ -262,102 +259,76 @@ function levelSelector(level) {
         </div>
       </div>
 
-      <!-- <div class="grid md:w-4/12 grid-cols-1 m-2 gap-5">
-        <div
-          v-for="rule in rules"
-          class="sm:w-full rounded-md shadow-md bg-red-300 p-0.5"
-          :key="rule.ruleNumber"
-        >
-          <div
-            :class="rule.correct ? 'bg-green-300' : 'bg-red-300'"
-            class="py-1 px-4 flex flex-row items-center gap-2"
-          >
-            <Correct
-              v-if="rule.correct"
-              class="fa-solid fa-check text-red-500 pt-1 text-2xl"
-            />
-            <i v-else class="fa-solid fa-xmark text-red-500 pt-1 text-2xl"></i>
-            <p class="font-itim text-xl">Rule {{ rule.ruleNumber }}</p>
-          </div>
-          <div
-            :class="rule.correct ? 'bg-green-100' : 'bg-red-100'"
-            class="p-4"
-          >
-            <p class="font-itim text-xl">{{ rule.message }}.</p>
-          </div>
-        </div> -->
-      <!-- </div> -->
       <!-- Timer display -->
       <!-- <div class="mt-5">
-        <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
-        button for test timer-->
+          <p class="font-istok text-xl">Timer {{ Displaytimeformat() }}</p>
+          button for test timer-->
       <!-- <button
-          @click="startTimer"
-          class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
-        >
-          Start Timer
-        </button>
-        <button
-          @click="stopTimer"
-          class="font-itim border border-black p-1 rounded-[5px]"
-        >
-          Stop Timer
-        </button>
-      </div> -->
-      <!-- <div
-        :class="
-          isOpen
-            ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
-            : 'h-[43px] transition-height duration-300 ease-in-out'
-        "
-        class="fixed bottom-0 overflow-hidden"
-      >
-        <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
-          <button class="flex flex-col items-center font-itim">
-            HOW TO PLAY GAME 🎮
-            <svg
-              width="24"
-              height="10"
-              viewBox="0 0 27 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line
-                x1="13.8826"
-                y1="15.2968"
-                x2="1.29294"
-                y2="2.70715"
-                stroke="black"
-                stroke-width="2"
-              />
-              <line
-                x1="13.0877"
-                y1="14.5877"
-                x2="26.0877"
-                y2="1.58769"
-                stroke="black"
-                stroke-width="2"
-              />
-            </svg>
+            @click="startTimer"
+            class="font-itim border border-black p-1 rounded-[5px] mr-[8px]"
+          >
+            Start Timer
           </button>
-        </div>
-        <div class="w-[300px] p-[10px]">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
-            magni repudiandae tempora eos nemo maiores, doloremque quis
-            obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
-            Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
-            incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
-            Maxime aliquam eos excepturi vel.
-          </p>
+          <button
+            @click="stopTimer"
+            class="font-itim border border-black p-1 rounded-[5px]"
+          >
+            Stop Timer
+          </button>
         </div> -->
+      <!-- <div
+          :class="
+            isOpen
+              ? 'h-[350px] bg-white rounded-[10px] transition-height duration-300 ease-in-out'
+              : 'h-[43px] transition-height duration-300 ease-in-out'
+          "
+          class="fixed bottom-0 overflow-hidden"
+        >
+          <div class="flex flex-col items-center p-1" @click="isOpen = !isOpen">
+            <button class="flex flex-col items-center font-itim">
+              HOW TO PLAY GAME 🎮
+              <svg
+                width="24"
+                height="10"
+                viewBox="0 0 27 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="13.8826"
+                  y1="15.2968"
+                  x2="1.29294"
+                  y2="2.70715"
+                  stroke="black"
+                  stroke-width="2"
+                />
+                <line
+                  x1="13.0877"
+                  y1="14.5877"
+                  x2="26.0877"
+                  y2="1.58769"
+                  stroke="black"
+                  stroke-width="2"
+                />
+              </svg>
+            </button>
+          </div>
+          <div class="w-[300px] p-[10px]">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
+              magni repudiandae tempora eos nemo maiores, doloremque quis
+              obcaecati totam, culpa dolorem sit eligendi adipisci laudantium ut!
+              Rem, quidem explicabo! Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Ea nesciunt corrupti minus facere ex. Distinctio
+              incidunt maxime provident rerum ad ea suscipit fuga ex praesentium!
+              Maxime aliquam eos excepturi vel.
+            </p>
+          </div> -->
     </div>
   </div>
 </template>
 
 <style scoped>
-  
 .background-color-hard {
   background: linear-gradient(104deg,
       #6e07f0 8.15%,
@@ -416,6 +387,7 @@ function levelSelector(level) {
       rgba(96, 22, 22, 0.83) 68.6%,
       rgba(29, 34, 45, 0.94) 89.63%);
 }
+
 @media (max-width: 600px) {
   .HowToPlay {
     height: 100vh;
@@ -511,39 +483,24 @@ function levelSelector(level) {
   .spy {
     width: 276px;
     height: 392px;
-  }
-}
-@media (min-width: 1201px) {
-  .hardBox {
-    height: 100vh;
-  }
-  .logo {
-    width: 441px;
-    height: 238px;
-  }
-  .spy {
-    width: 200px;
-    height: 380px;
-    position: absolute;
-    margin-top: 60px;
-    left: 13%;
-  }
-
-  .button {
+    grid-row: 4;
+    grid-column: 1;
+    display: flex;
+    justify-items: center;
+    justify-self: center;
   }
 
   .HowToPlayFont {
-    position: absolute;
-    bottom: 5%;
+    grid-row: 5;
+    grid-column: 2;
     text-align: center;
-    left: 45%;
   }
 
   .HowToPlay {
-    position: absolute;
-    bottom: -10%;
-    right: 5%;
-    left: 5%;
+    grid-row: 6;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    text-align: center;
   }
 }
 </style>
